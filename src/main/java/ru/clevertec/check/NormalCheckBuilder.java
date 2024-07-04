f16500dd55b8e96f6798987f4ecac49705571ec0
alyaromin <alyaromin@gmail.com>
2024-07-04 07:02:35 +0300

    feat: start implement NormalCheckBuilder.java, create Product.java

--- src/main/java/ru/clevertec/check/NormalCheckBuilder.java
+++ src/main/java/ru/clevertec/check/NormalCheckBuilder.java
@@ -1,7 +1,26 @@
 package main.java.ru.clevertec.check;
 
-public class NormalCheckBuilder implements CheckBuilder {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+public class NormalCheckBuilder implements CheckBuilder {
+	private List <Product> products = new ArrayList<Product>();
+	private int discount = 0;
+	private float balance;
+	
+	public CheckBuilder addProducts(Map<Integer, Integer> requestedProducts) {
+		return this;
+	}
+	
+	public CheckBuilder setDiscount(int discountCard) {
+		return this;
+	}
+	
+	public CheckBuilder setBalance(float balance) {
+		return this;
+	}
+	
 	@Override
 	public String build() {
 		// TODO Auto-generated method stub
---
+++ src/main/java/ru/clevertec/check/Product.java
@@ -0,0 +1,5 @@
+package main.java.ru.clevertec.check;
+
+public class Product {
+
+}
